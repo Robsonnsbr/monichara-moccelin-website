2fe99ece92c87f54e4b9206e81fd9d308fd9fb70
robsonnsbr <robsonnsbr@gmail.com>
2025-02-04 17:17:13 -0300

    layout: add styles

--- src/components/specialties/content/index.tsx
+++ src/components/specialties/content/index.tsx
@@ -1,10 +1,13 @@
 function SpecialtyContent({ services }: { services: any[] }) {
   return (
-    <ul className="list-none space-y-4">
+    <ul className="list-none space-y-4 lg:w-4/5">
       {services.map((service, index) => (
-        <li key={index} className="mb-4">
-          <h3 className="text-xl font-bold">{service.title}</h3>
-          <p className="text-gray-600">{service.description}</p>
+        <li
+          key={index}
+          className="p-2 sm:p-6 bg-gradient-to-r  from-extraLightBlueGray to-lightBlueGray rounded-md space-x-4 text-deepIndigoBlue"
+        >
+          <h3 className="text-xl font-bold mb-2">{service.title}</h3>
+          <p className="text-md">{service.description}</p>
         </li>
       ))}
     </ul>
