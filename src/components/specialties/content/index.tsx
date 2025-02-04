82ff5d973f0404da42b5f7d7ff0ca81367d09b8d
robsonnsbr <robsonnsbr@gmail.com>
2025-02-04 17:20:50 -0300

    fix: ....

--- src/components/specialties/content/index.tsx
+++ src/components/specialties/content/index.tsx
@@ -4,7 +4,7 @@ function SpecialtyContent({ services }: { services: any[] }) {
       {services.map((service, index) => (
         <li
           key={index}
-          className="p-2 sm:p-6 bg-gradient-to-r  from-extraLightBlueGray to-lightBlueGray rounded-md space-x-4 text-deepIndigoBlue"
+          className="p-2 sm:p-6 bg-extraLightBlueGray rounded-md space-x-4 text-deepIndigoBlue"
         >
           <h3 className="text-xl font-bold mb-2">{service.title}</h3>
           <p className="text-md">{service.description}</p>
