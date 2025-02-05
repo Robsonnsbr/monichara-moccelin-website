3476668314420938f120aa847081f7b901cefaf5
robsonnsbr <robsonnsbr@gmail.com>
2025-02-05 14:49:59 -0300

    fix: ...

--- src/components/home/clientTestimonials/ElfSightWidget.tsx
+++ src/components/home/clientTestimonials/ElfSightWidget.tsx
@@ -4,14 +4,11 @@ import Script from 'next/script';
 const ElfSightWidget = () => {
   return (
     <>
-      <Script
-        src="https://static.elfsight.com/platform/platform.js"
-        async
-      ></Script>
+      <Script src="https://static.elfsight.com/platform/platform.js" async />
       <div
         className="elfsight-app-c3128f2e-158e-46d3-a705-4a56fd12078a font-poppins py-10 sm:py-20"
         data-elfsight-app-lazy
-      ></div>
+      />
     </>
   );
 };
