9d3cd18ffbce67f5ebe19c002489d849869fb4be
robsonnsbr <robsonnsbr@gmail.com>
2025-02-05 18:32:17 -0300

    fix: ...

---
+++ src/components/home/clientTestimonials/loading.tsx
@@ -0,0 +1,35 @@
+export default function Loading() {
+  return (
+    <div className="space-y-4">
+      {/* Comentário 1 */}
+      <div className="flex items-center space-x-4 animate-pulse">
+        <div className="h-12 w-12 bg-gray-300 rounded-full" />
+        <div className="flex-1 space-y-2">
+          <div className="h-4 bg-gray-300 rounded w-3/4" />
+          <div className="h-3 bg-gray-300 rounded w-full" />
+          <div className="h-3 bg-gray-300 rounded w-5/6" />
+        </div>
+      </div>
+
+      {/* Comentário 2 */}
+      <div className="flex items-center space-x-4 animate-pulse">
+        <div className="h-12 w-12 bg-gray-300 rounded-full" />
+        <div className="flex-1 space-y-2">
+          <div className="h-4 bg-gray-300 rounded w-3/4" />
+          <div className="h-3 bg-gray-300 rounded w-full" />
+          <div className="h-3 bg-gray-300 rounded w-5/6" />
+        </div>
+      </div>
+
+      {/* Comentário 3 */}
+      <div className="flex items-center space-x-4 animate-pulse">
+        <div className="h-12 w-12 bg-gray-300 rounded-full" />
+        <div className="flex-1 space-y-2">
+          <div className="h-4 bg-gray-300 rounded w-3/4" />
+          <div className="h-3 bg-gray-300 rounded w-full" />
+          <div className="h-3 bg-gray-300 rounded w-5/6" />
+        </div>
+      </div>
+    </div>
+  );
+}
