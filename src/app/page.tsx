804571070ee42bcecb0972824fe5d8ed66853b8e
robsonnsbr <robsonnsbr@gmail.com>
2025-02-26 15:54:17 -0300

    fix: ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Metadata } from 'node_modules/next';
 import Main from 'src/components/common/Main';
 import AboutSummary from 'src/components/home/aboutSummary';
 import Testimonials from 'src/components/home/clientTestimonials';
 import ListServicesHome from 'src/components/home/listServices/ListServicesHome';
 import PosterHome from 'src/components/home/poster/PosterHome';
 import TopHome from 'src/components/home/topHome/TopHome';
+
+export const metadata: Metadata = {
+  title: 'Home'
+};
 
 export default function Home() {
   return (
